8bfb32c6647a9e2076df5f62eccb2d73860f7d68
Ayca Tuzmen <9531002-AycaTuzmen@users.noreply.replit.com>
2024-03-14 03:28:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var newScreenButton
+
+function setup() {
+  createCanvas(400, 400);
+  background("red")
+  newScreenButton = createButton("New Screen");
+  newScreenButton.position(50,50)
+  newScreenButton.mousePressed(clickedNewScreen);
+}
+
+function draw() {
+
+}
+
+function clickedNewScreen(){
+  //window.open("https://NewPageswithNewFile2.aycatuzmen.repl.co", "_self")
+  //window.open("https://95dc9fa3-111d-4f01-87bc-9fa7119dca16-00-1j3oyk5cngcsm.riker.repl.co/","_self");
+  //window.open("https://replit.com/@AycaTuzmen/NewPages_withNewFile2/", "_self")
+  window.open("https://95dc9fa3-111d-4f01-87bc-9fa7119dca16-00-1j3oyk5cngcsm.riker.replit.dev/", "_self")
+}
